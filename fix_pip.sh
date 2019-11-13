058e255b781b22942e37efe4dfbfaf2a806c8561
Evan Ott <evan1533@vt.edu>
2019-11-13 18:16:49 -0500

    Fixed python helix script

---
+++ fix_pip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://bootstrap.pypa.io/get-pip.py | python3 - --user --force-reinstall
--- nice.py
+++ nice.py
@@ -3,7 +3,7 @@
 import math
 import time
 
-i = 0;
+i = 0
 """
 a = 24
 om = 0.15
@@ -12,32 +12,32 @@ sleep = 0.05
 args = []
 """
 
+
 def readVals():
-	global a, om, off, sleep
-	helix = open("helix.txt","r");
-	for line in helix:
-		args = line.split(",");
-		a = int(args[0])
-		om = float(args[1])
-		off = int(args[2])
-		sleep = float(args[3].strip('\n'))
-	# print(args)
-	helix.close()
+    global a, om, off, sleep
+    helix = open("helix.txt", "r")
+    for line in helix:
+        args = line.split(",")
+        a = int(args[0])
+        om = float(args[1])
+        off = int(args[2])
+        sleep = float(args[3].strip('\n'))
+        # print(args)
+    helix.close()
+
 
 while True:
-	global a, om, off, sleep
-	if i%10 == 0:
-		readVals()
-	for j in range(0, math.floor(a*math.sin(i*om))+off):
-		print(" ", end='');
-	print('\033[38;5;201mo')
-	for j in range(0, math.floor(a*math.sin(i*om + math.pi))+off):
-		print(" ", end='')
-	print('\033[38;5;10mo');
-	for j in range(0, math.floor(a*math.cos(i*om + math.pi))+off):
-		print(" ", end='')
-	print('\033[38;5;11mo');
-	i+=1
-	time.sleep(sleep);
-
-
+    global a, om, off, sleep
+    if i % 10 == 0:
+        readVals()
+    for j in range(0, math.floor(a*math.sin(i*om))+off):
+        print(" ", end='')
+    print('\033[38;5;201mo')
+    for j in range(0, math.floor(a*math.sin(i*om + math.pi))+off):
+        print(" ", end='')
+    print('\033[38;5;10mo')
+    for j in range(0, math.floor(a*math.cos(i*om + math.pi))+off):
+        print(" ", end='')
+    print('\033[38;5;11mo')
+    i += 1
+    time.sleep(sleep)
