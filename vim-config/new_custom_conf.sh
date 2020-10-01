ed979cee1628b84c73c09e0778bbdb82ca927f41
Evan Ott <evan1533@vt.edu>
2020-10-01 17:53:04 -0400

    vim-config/new_custom_conf.sh: tweaked my custom vim setup script

--- vim-config/new_custom_conf.sh
+++ vim-config/new_custom_conf.sh
@@ -2,6 +2,9 @@
 
 # sudo apt remove vim vim-runtime gvim
 
+sudo apt build-dep -y vim-gtk
+sudo apt install -y python3
+
 ./configure --enable-fail-if-missing \
             --with-compiledby=evan1533 \
             --with-features=huge \
@@ -17,8 +20,15 @@
             --enable-rubyinterp=yes \
             --with-python3-config-dir=$(python3-config --configdir) \
             --prefix=/usr/local \
- && make distclean && make VIMRUNTIMEDIR=/usr/local/share/vim/vim82 -j8
+ && make distclean \
+ && make VIMRUNTIMEDIR=/usr/local/share/vim/vim82 -j$(nproc) \
+ && make install
 
 
-VERSION=`git describe --tags --abbrev=0 HEAD`
-sudo checkinstall --pkgname 'vim' --pkgversion '${VERSION#v}' --pkgrelease '8' --pkgsource 'https://github.com/vim/vim' --provides 'vim'
+# VERSION=`git describe --tags --abbrev=0 HEAD`
+# sudo checkinstall --pkgname 'vim' --pkgversion '${VERSION#v}' --pkgrelease '8' --pkgsource 'https://github.com/vim/vim' --provides 'vim'
+
+VIM=`which vim`
+sudo update-alternatives --install /usr/bin/vim vim $VIM 90
+sudo update-alternatives --install /usr/bin/vi vi $VIM 90
+sudo update-alternatives --install /usr/bin/editor editor $VIM 90
