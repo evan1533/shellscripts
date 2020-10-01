4bf012c5a7b01fffa420524b4f1a397102b1cc50
Evan Ott <evan1533@vt.edu>
2020-10-01 17:46:44 -0400

    fix_mpsyt.sh: only open vimdiff if the two files actually differ

--- fix_mpsyt.sh
+++ fix_mpsyt.sh
@@ -4,6 +4,12 @@ API_KEY="AIzaSyDD-vlcEttzl8meaq0nPhSolz_DuZHQJyg"
 cat  $CONFIG_FILE\
     | sed "s/AIzaSyCIM4EzNqi1in22f4Z3Ru3iYvLaY8tc3bo/$API_KEY/g" > ~/new_config.py
 
-vimdiff ~/new_config.py $CONFIG_FILE
-rm ~/new_config.py
-rm ~/.config/mps-youtube/cache_py_*
+if [[ ! -z $(diff ~/new_config.py $CONFIG_FILE) ]]
+then
+    vimdiff ~/new_config.py $CONFIG_FILE
+fi
+
+rm -f ~/new_config.py
+rm -f ~/.config/mps-youtube/cache_py_*
+
+# python3 -V 2>&1 | tr [:upper:] [:lower:] | tr -d   | sed 's/\.[0-9]\+$//'
